3798ae4d377f3619b4b7ea17fe6eca71d5a88a68
Elineely <sooleeny@naver.com>
2021-07-28 16:50:39 +0900

    show what the day today is using simple ejs

--- app.js
+++ app.js
@@ -4,14 +4,43 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/",function(req, res){
 
   var today = new Date();
-  if(today.getDay === 0 || today.getDay === 6){
-    res.send("Hooray! It's a weekend");
-  }else{
-    res.send("Boo.. It's another weekday");
+  var day = today.getDay();
+  var result = "";
+
+  switch (day) {
+    case 0 :
+    result = "Sunday";
+    break;
+    case 1 :
+    result = "Monday";
+    break;
+    case 2 :
+    result = "Tuesday";
+    break;
+    case 3 :
+    result = "Wendnesday";
+    break;
+    case 4 :
+    result = "Thursday";
+    break;
+    case 5 :
+    result = "Friday";
+    break;
+    case 6 :
+    result = "Saturday";
+    break;
+
+    default :
+    console.log("The result is " + result +".");
   }
+
+    res.render('list', { kindOfDay : result });
+
 });
 
 app.listen(3000, function(req, res){
